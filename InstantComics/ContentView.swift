512b53d24a42d38f33935b27f2fe1a12ed4f9a3f
Kjetil <kjetil.dir@gmail.com>
2021-03-15 21:39:53 +0100

    Initial Commit

---
+++ InstantComics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstantComics
+//
+//  Created by Kjetil Skyldstad Bjelldokken on 15/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstantComics/InstantComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantComicsApp.swift
+//  InstantComics
+//
+//  Created by Kjetil Skyldstad Bjelldokken on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstantComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
